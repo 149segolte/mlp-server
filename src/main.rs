a853190a7ed94503c3310271b874e6e2f60aa3bc
149segolte <37300847+149segolte@users.noreply.github.com>
2023-09-07 11:03:17 +0530

    Setup axum project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, routing::post, Router};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new()
+        .route("/", get(projects))
+        .route("/new", post(new_project));
+
+    // run it with hyper on localhost:3000
+    println!("Server running on localhost:3000");
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
+
+async fn projects() {}
+async fn new_project() {}
